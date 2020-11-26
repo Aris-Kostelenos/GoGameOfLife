c9b6e421484b4e8ff4d26398cb2f7dffab680528
ariskostelenos <gp19712@bristol.ac.uk>
2020-11-26 16:13:58 +0000

    parallelisation attempt 1

--- gol/distributor.go
+++ gol/distributor.go
@@ -15,6 +15,17 @@ type distributorChannels struct {
 	ioOutput   chan<- uint8
 }
 
+type workerParams struct {
+	id                       int
+	events                   chan<- Event
+	imagePartWidthStartpoint int
+	imagePartWidth           int
+	imagePartHeight          int
+	turns                    int
+	threads                  int
+	//	answer          chan<- [][]uint8
+}
+
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
 
@@ -23,25 +34,68 @@ func distributor(p Params, c distributorChannels) {
 	c.ioFilename <- fmt.Sprintf("%vx%v", p.ImageWidth, p.ImageHeight)
 
 	grid := make([][]uint8, p.ImageHeight)
+
 	for row := 0; row < p.ImageHeight; row++ {
-		thisRow := make([]uint8, p.ImageWidth)
+		grid[row] = make([]uint8, p.ImageWidth)
 		for cell := 0; cell < p.ImageWidth; cell++ {
-			value := <-c.ioInput
-			thisRow[cell] = value
-			// grid[row][cell] = value
+			grid[row][cell] = <-c.ioInput
 			c.events <- CellFlipped{0, util.Cell{X: cell, Y: row}}
 		}
-		grid[row] = thisRow
 	}
+
 	// TODO: For all initially alive cells send a CellFlipped Event.
 
 	turn := 0
 
 	// TODO: Execute all turns of the Game of Life.
-	ap := golParams{c.events, p.ImageWidth, p.ImageHeight}
+	ap := workerParams{0, c.events, 0, p.ImageWidth, p.ImageHeight, p.Turns, p.Threads}
+
+	wp := workerParams{0, c.events, 0, 0, p.ImageHeight, p.Turns, p.Threads}
+
+	workerChannels := make([]chan uint8, 2*p.Threads+1)
+	for i := range workerChannels {
+		workerChannels[i] = make(chan uint8, p.ImageHeight)
+	}
+	statusChannels := make([]chan bool, p.Threads)
+	for i := range statusChannels {
+		statusChannels[i] = make(chan bool)
+	}
+
+	wp.imagePartWidth = p.ImageWidth / p.Threads
+
+	if p.ImageWidth%p.Threads == 0 {
+
+		// basically for each go routine we need two channels where it is going to send the edges of the results it gets
+
+		for i := 0; i < p.Threads; i++ {
+			wp.id = i
+			wp.imagePartWidthStartpoint = i * wp.imagePartWidth
+			go workerGoroutine(wp, grid, workerChannels, statusChannels)
+		}
+	} else {
+		fmt.Println("uuuhhh fuk...........")
+	}
+
 	for turn = 0; turn < p.Turns; turn++ {
-		grid = calculateNextState(ap, grid)
+		for i := 0; i < p.Threads; i++ {
+			h := <-statusChannels[i]
+			if h == true {
+			}
+		}
 		c.events <- TurnComplete{turn}
+
+	}
+
+	/*
+		for turn = 0; turn < p.Turns; turn++ {
+			grid = calculateNextState(ap, grid)
+			c.events <- TurnComplete{turn}
+		}
+	*/
+	for i := 0; i < p.Threads; i++ {
+		h := <-statusChannels[i]
+		if h == false {
+		}
 	}
 
 	c.events <- FinalTurnComplete{turn, calculateAliveCells(ap, grid)}
--- gol/worker.go
+++ gol/worker.go
@@ -1,83 +1,249 @@
 package gol
 
 import (
-	// "fmt"
+	"time"
+
 	"uk.ac.bris.cs/gameoflife/util"
 )
 
-type golParams struct {
-	events      chan<- Event
-	imageWidth  int
-	imageHeight int
+type borders struct {
+	top    []uint8
+	bottom []uint8
 }
 
-func calculateNextState(p golParams, world [][]uint8) [][]uint8 {
-	world1 := make([][]uint8, p.imageWidth)
-	for z := 0; z < p.imageWidth; z++ {
-		world1[z] = make([]uint8, p.imageHeight)
+func calculateNextState(p workerParams, world [][]uint8, borders borders) [][]uint8 {
+	oldWorld := make([][]uint8, p.imagePartWidth+2)
+	for z := 0; z < p.imagePartWidth+2; z++ {
+		oldWorld[z] = make([]uint8, p.imagePartHeight)
 	}
-	for row := range world {
-		for cell := range world[row] {
+	for i := 0; i < p.imagePartHeight; i++ {
+		for z := 1; z <= p.imagePartWidth; z++ {
+			oldWorld[z][i] = world[i][z-1]
+		}
+		oldWorld[1][i] = borders.top[i]
+		oldWorld[p.imagePartWidth+1][i] = borders.bottom[i]
+	}
+
+	newWorld := make([][]uint8, p.imagePartWidth)
+	for z := 0; z < p.imagePartWidth; z++ {
+		newWorld[z] = make([]uint8, p.imagePartHeight)
+	}
+
+	for row := range oldWorld {
+		for cell := range oldWorld[row] {
 			var a int
 			var b int
 			var c int
 			var d int
 
 			if row == 0 {
-				a = p.imageWidth - 1
+				a = p.imagePartWidth + 1
 			} else {
 				a = row - 1
 			}
 			if cell == 0 {
-				c = p.imageHeight - 1
+				c = p.imagePartHeight - 1
 			} else {
 				c = cell - 1
 			}
-			if row == p.imageWidth-1 {
+			if row == p.imagePartWidth+1 {
 				b = 0
 			} else {
 				b = row + 1
 			}
-			if cell == p.imageHeight-1 {
+			if cell == p.imagePartHeight-1 {
 				d = 0
 			} else {
 				d = cell + 1
 			}
 
-			x := ((world[a][c] / 255) + (world[a][cell] / 255) + (world[a][d] / 255) + (world[row][c] / 255) + (world[row][d] / 255) + (world[b][c] / 255) + (world[b][cell] / 255) + (world[b][d] / 255))
+			x := (oldWorld[a][c] + oldWorld[a][cell] + oldWorld[a][d] + oldWorld[row][c] + oldWorld[row][d] + oldWorld[b][c] + oldWorld[b][cell] + oldWorld[b][d]) / 255
+			if row != 0 && row != p.imagePartWidth+1 {
+				if oldWorld[row][cell] == 0 {
+					if x == 3 {
+						newWorld[row-1][cell] = 255
+						p.events <- CellFlipped{255, util.Cell{X: row, Y: cell}}
+					} else {
+						newWorld[row-1][cell] = 0
+					}
+				}
+
+				if oldWorld[row][cell] == 255 {
+					if x == 3 || x == 2 {
+						newWorld[row-1][cell] = 255
+					} else {
+						newWorld[row-1][cell] = 0
+						p.events <- CellFlipped{0, util.Cell{X: row, Y: cell}}
+					}
+				}
+
+			}
+
+		}
+	}
+	return newWorld
+}
+
+/*
+func calculateNextStatePart(p workerParams, world [][]uint8, b borders) [][]uint8 {
+
+	//this shit receives the borders from the neighbouring goroutines using borders in order to calculet if the shit cell are alive or fukcing ded
+
+	NewWorld := make([][]uint8, p.imagePartWidth)
+
+	for z := 0; z < p.imagePartWidth; z++ {
+		NewWorld[z] = make([]uint8, p.imagePartHeight)
+	}
+	for row := range world {
+		for cell := range world[row] {
+			var left int
+			var right int
+			var x uint8
+
+			if cell == 0 {
+				left = p.imagePartHeight - 1
+			} else {
+				left = cell - 1
+			}
+			if cell == p.imagePartHeight-1 {
+				right = 0
+			} else {
+				right = cell + 1
+			}
+
+			// if the cell it calculates is on top or bottom instead of using world it uses the fucking border arrays top and bottom ok
+
+			if row == 0 {
+				x = ((b.top[left] + b.top[cell] + b.top[right] + world[row][left] + world[row][right] + world[row+1][left] + world[row+1][cell] + world[row+1][right]) / 255)
+
+			} else if row == p.imagePartWidth-1 {
+				x = ((world[row-1][left] + world[row-1][cell] + world[row-1][right] + world[row][left] + world[row][right] + b.bottom[left] + b.bottom[cell] + b.bottom[right]) / 255)
+
+			} else {
+				x = ((world[row-1][left] + world[row-1][cell] + world[row-1][right] + world[row][left] + world[row][right] + world[row+1][left] + world[row+1][cell] + world[row+1][right]) / 255)
+
+			}
+
+			//x := ((world[above][left] / 255) + (world[above][cell] / 255) + (world[above][right] / 255) + (world[row][left] / 255) + (world[row][right] / 255) + (world[below][left] / 255) + (world[below][cell] / 255) + (world[below][right] / 255))
 
 			if world[row][cell] == 0 {
 				if x == 3 {
-					world1[row][cell] = 255
+					NewWorld[row][cell] = 255
 					p.events <- CellFlipped{255, util.Cell{X: row, Y: cell}}
 				} else {
-					world1[row][cell] = 0
+					NewWorld[row][cell] = 0
 				}
 			}
 
 			if world[row][cell] == 255 {
 				if x == 3 || x == 2 {
-					world1[row][cell] = 255
+					NewWorld[row][cell] = 255
 				} else {
-					world1[row][cell] = 0
-					p.events <- CellFlipped{0, util.Cell{X: row, Y: cell}}
+					NewWorld[row][cell] = 0
+					//p.events <- CellFlipped{0, util.Cell{X: row, Y: cell}}
 				}
 			}
 		}
 	}
-	return world1
+	return NewWorld
 }
 
-func calculateAliveCells(p golParams, world [][]uint8) []util.Cell {
+*/
+
+func calculateAliveCells(p workerParams, world [][]uint8) []util.Cell {
 	a := make([]util.Cell, 0)
 	k := 0
 	for row := range world {
 		for cell := range world[row] {
 			if world[cell][row] == 255 {
-				a = append(a, util.Cell{X: row, Y: cell})
+				a = append(a, util.Cell{X: cell, Y: row})
 				k++
 			}
 		}
 	}
 	return a
+}
+
+func workerGoroutine(p workerParams, world [][]uint8, channels []chan uint8, statusChannel []chan bool) {
+
+
+	gridPart := make([][]uint8, p.imagePartWidth)
+	for row := 0; row < p.imagePartHeight; row++ {
+		gridPart[row] = make([]uint8, p.imagePartWidth)
+		for cell := 0; cell < p.imagePartWidth; cell++ {
+			gridPart[row][cell] = world[row+p.imagePartWidthStartpoint][cell]
+		}
+	}
+
+	top := make([]uint8, p.imagePartHeight)
+	bottom := make([]uint8, p.imagePartHeight)
+	temp := borders{top, bottom}
+
+	for i := 0; i < p.imagePartHeight; i++ {
+
+		if p.id != 0 && p.id != p.threads-1 {
+			top[i] = world[p.imagePartWidthStartpoint-1][i]
+			bottom[i] = world[p.imagePartWidthStartpoint+p.imagePartWidth-1][i]
+		} else if p.id == 0 {
+			top[i] = world[(p.imagePartWidth*p.threads)-1][i]
+			bottom[i] = world[p.imagePartWidthStartpoint+p.imagePartWidth-1][i]
+		} else {
+			top[i] = world[p.imagePartWidthStartpoint-1][i]
+			bottom[i] = world[0][i]
+		}
+
+	}
+
+	for turns := 0; turns < p.turns; turns++ {
+		gridPart = calculateNextState(p, gridPart, temp)
+		time.Sleep(time.Millisecond)
+		statusChannel[p.id] <- true
+
+		//channels[2*p.id] <- gridPart[0]
+		//channels[2*p.id+1] <- gridPart[p.imagePartWidth]
+		//fmt.Println("I reached this point!")
+		if p.id != 0 && p.id != p.threads-1 {
+			sendUintSliceToChannel(p.imagePartHeight, channels[2*p.id], gridPart[0])
+			sendUintSliceToChannel(p.imagePartHeight, channels[2*p.id+1], gridPart[p.imagePartWidth])
+			copyUintSliceFromChannel(p.imagePartHeight, channels[2*p.id-1], top)
+			copyUintSliceFromChannel(p.imagePartHeight, channels[2*p.id+2], bottom)
+		} else if p.id == 0 && p.id == p.threads-1 {
+			sendUintSliceToChannel(p.imagePartHeight, channels[0], gridPart[0])
+			sendUintSliceToChannel(p.imagePartHeight, channels[1], gridPart[p.imagePartWidth-1])
+			time.Sleep(time.Millisecond)
+			copyUintSliceFromChannel(p.imagePartHeight, channels[1], top)
+			copyUintSliceFromChannel(p.imagePartHeight, channels[0], bottom)
+		} else if p.id == 0 {
+			//channels[2*p.id] <- gridPart[0]
+			//	channels[2*p.id+1] <- gridPart[p.imagePartWidth]
+			//	top = <-channels[p.threads]
+			//	bottom = <-channels[2]
+		} else {
+			//	channels[2*p.id] <- gridPart[0]
+			//	channels[2*p.id+1] <- gridPart[p.imagePartWidth]
+			//	top = <-channels[2*p.id-1]
+			//	bottom = <-channels[0]
+		}
+
+	}
+
+	for row := 0; row < p.imagePartHeight; row++ {
+		for cell := 0; cell < p.imagePartWidth; cell++ {
+			world[cell][row+p.imagePartWidthStartpoint] = gridPart[row][cell]
+		}
+	}
+	statusChannel[p.id] <- false
+
+}
+
+func copyUintSliceFromChannel(size int, channel chan uint8, slice []uint8) {
+	for i := 0; i < size; i++ {
+		slice[i] = <-channel
+	}
+}
+
+func sendUintSliceToChannel(size int, channel chan uint8, slice []uint8) {
+	for i := 0; i < size; i++ {
+		channel <- slice[i]
+	}
 }
